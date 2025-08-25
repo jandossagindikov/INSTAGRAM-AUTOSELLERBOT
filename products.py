19f3290ea4ea54b972391f82484f59b8f013c4a0
Jandos Sagindikov <zandossagindikov1@gmail.com>
2025-08-25 17:09:03 +0500

    update

--- products.py
+++ products.py
@@ -3,14 +3,14 @@ products = [
     {
         "media_id": "17991437924683241",
         "media_url": "https://www.instagram.com/reel/DNNj6RSP7_t/",
-        "buy_link": "https://uzum.uz/uz/product/rasshiritel-dlya-nosa-oq---5-1660905?skuId=5662019",
+        "buy_link": "https://uzum.uz/product/1660905?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/d0e434i7s4fo7mq89ujg/original.jpg",
         "title": "Burun kengaytirgich"
     },
     {
         "media_id": "18137348917428416",
         "media_url": "https://www.instagram.com/p/DNlU4nwqe39/?img_index=1",
-        "buy_link": "https://uzum.uz/uz/product/windows-ornatilgan-ofis-metall-kulrang---291-1694031?skuId=5780830",
+        "buy_link": "https://uzum.uz/product/1694031?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/d24ccst2lln1ps3bs800/original.jpg",
         "title": "iTech noutbuki"
     }
