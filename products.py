4dde6eefa836aa63ccdfa8d4817be2171c08cb40
Jandos Sagindikov <zandossagindikov1@gmail.com>
2025-09-07 13:17:53 +0500

    update project

--- products.py
+++ products.py
@@ -5,14 +5,17 @@ products = [
         "media_url": "https://www.instagram.com/reel/DNNj6RSP7_t/",
         "buy_link": "https://uzum.uz/product/1660905?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/d0e434i7s4fo7mq89ujg/original.jpg",
-        "title": "Burun kengaytirgich"
+        "title": "Burun kengaytirgich",
+        "comments": ["+","++","+++","++++","+++++","++++++","+++++++","++++++++","+++++++++","++++++++++","+++++++++++","++++++++++++","+++++++++++++","++++++++++++++","+++++++++++++++","++++++++++++++++","+++++++++++++++++","++++++++++++++++++","+++++++++++++++++++"]
     },
     {
         "media_id": "18050406764627625",
         "media_url": "https://www.instagram.com/p/DOEPQu9im04/",
         "buy_link": "https://uzum.uz/product/1163247?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/cqht46ksslomdvnib6q0/original.jpg",
-        "title": "Tungi chiroq porlayotgan oy"
+        "title": "Tungi chiroq porlayotgan oy",
+        "comments": ["+","++","+++","++++","+++++","++++++","+++++++","++++++++","+++++++++","++++++++++","+++++++++++","++++++++++++","+++++++++++++","++++++++++++++","+++++++++++++++","++++++++++++++++","+++++++++++++++++","++++++++++++++++++","+++++++++++++++++++"]
+
         # 2025.09.1  
     },
     {
@@ -20,7 +23,9 @@ products = [
         "media_url": "https://www.instagram.com/reel/DN06BxJwJnV/",
         "buy_link": "https://uzum.uz/product/1884673?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/d2dp9o34eu2mnqe5hm70/original.jpg",
-        "title": "Sabzavot tozalag'ich"
+        "title": "Sabzavot tozalag'ich",
+        "comments": ["+","++","+++","++++","+++++","++++++","+++++++","++++++++","+++++++++","++++++++++","+++++++++++","++++++++++++","+++++++++++++","++++++++++++++","+++++++++++++++","++++++++++++++++","+++++++++++++++++","++++++++++++++++++","+++++++++++++++++++"]
+
         # 2025.09.2  
     },
     {
@@ -28,7 +33,9 @@ products = [
         "media_url": "https://www.instagram.com/p/DOJSNI2iiL4/",
         "buy_link": "https://uzum.uz/product/1272580?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/cs20qjksslojjk5rogcg/original.jpg",
-        "title": "Elektr idish yuvish cho'tkasi"
+        "title": "Elektr idish yuvish cho'tkasi",
+        "comments": ["+","++","+++","++++","+++++","++++++","+++++++","++++++++","+++++++++","++++++++++","+++++++++++","++++++++++++","+++++++++++++","++++++++++++++","+++++++++++++++","++++++++++++++++","+++++++++++++++++","++++++++++++++++++","+++++++++++++++++++"]
+
         # 2025.09.3  
     },
     {
@@ -36,7 +43,9 @@ products = [
         "media_url": "https://www.instagram.com/p/DOK-VEJCjTy/",  #<-- instagramga joylagan narsa qoyish kerak
         "buy_link": "https://uzum.uz/product/1739990?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/d0srvl33uvph50a0ielg/original.jpg",
-        "title": "Sovun pufakchalari generatori dinozavr shaklida"
+        "title": "Sovun pufakchalari generatori dinozavr shaklida",
+        "comments": ["+","++","+++","++++","+++++","++++++","+++++++","++++++++","+++++++++","++++++++++","+++++++++++","++++++++++++","+++++++++++++","++++++++++++++","+++++++++++++++","++++++++++++++++","+++++++++++++++++","++++++++++++++++++","+++++++++++++++++++"]
+
         # 2025.09.4  
     },
     {
@@ -44,7 +53,9 @@ products = [
         "media_url": "https://www.instagram.com/p/DOJSNI2iiL4/",
         "buy_link": "https://uzum.uz/product/1222343?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/crc8mem0t1lqb8aq6aj0/original.jpg",
-        "title": "G'ilof URR Smart Cover Corning"
+        "title": "G'ilof URR Smart Cover Corning",
+        "comments": ["+","++","+++","++++","+++++","++++++","+++++++","++++++++","+++++++++","++++++++++","+++++++++++","++++++++++++","+++++++++++++","++++++++++++++","+++++++++++++++","++++++++++++++++","+++++++++++++++++","++++++++++++++++++","+++++++++++++++++++"]
+
         # 2025.09.6 
     },
     {
@@ -52,7 +63,9 @@ products = [
         "media_url": "https://www.instagram.com/p/DOSmlnlCk3D/",
         "buy_link": "https://uzum.uz/product/1504386?utm_source=sharing&utm_medium=product_page_android&utm_campaign=native",
         "image_url": "https://images.uzum.uz/cur1hh3vgbkm5ehfr7q0/original.jpg",
-        "title": "Professional akkumuliyatorli bog‘ sekatori"
+        "title": "Professional akkumuliyatorli bog‘ sekatori",
+        "comments": ["+","++","+++","++++","+++++","++++++","+++++++","++++++++","+++++++++","++++++++++","+++++++++++","++++++++++++","+++++++++++++","++++++++++++++","+++++++++++++++","++++++++++++++++","+++++++++++++++++","++++++++++++++++++","+++++++++++++++++++"]
+
         # 2025.09.7
     }
 ]
